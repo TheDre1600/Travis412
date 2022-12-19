59f45465e4beeaa53f970f10f56748240e9bd003
AndreJackson <andre.jackson0714@gmail.com>
2022-12-18 19:54:45 -0500

    Error Commit

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,23 +9,6 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(1, 1), 0);
 	}
-	
-	public void testMinus() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(5, 3), 2);
-	}
-	public void testMultiply() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(2, 1), 2);
-	}
-	public void testDivide() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(4, 2), 2);
-	}
-	public void testDivideZero() {
-	SimpleCalculator calc = new SimpleCalculator();
-	assertEquals(calc.add(1, 0), 0);
-}
 }
