e884933b5ba9e59d489d95bf0d3502cd53a5351f
AndreJackson <andre.jackson0714@gmail.com>
2022-12-18 20:01:46 -0500

    COM

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,20 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 0);
 	}
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(5, 3), 2);
+	}
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(2, 1), 2);
+	}
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(4, 2), 2);
+	}
+	public void testDivideZero() {
+	SimpleCalculator calc = new SimpleCalculator();
+	assertEquals(calc.add(1, 0), 0);
+}
 }
